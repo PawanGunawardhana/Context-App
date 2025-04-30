34f26d5a378202a24463310c1a159121b6dc69d0
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-04-30 15:16:03 +0530

    Consumed multiple contexts and updated cheat sheet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context; //context eka use karanna one nam this.context kiyana property eka use karanna one.
-          const theme = isLightTheme ? light : dark; //isLightTheme kiyana property eka true nam light theme eka use karanna one. naththam dark theme eka use karanna one.
-
-          return (
-            <nav style={{ color: theme.syntax, background: theme.ui }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isLightTheme, light, dark } = themeContext; //context eka use karanna one nam this.context kiyana property eka use karanna one.
+              const theme = isLightTheme ? light : dark; //isLightTheme kiyana property eka true nam light theme eka use karanna one. naththam dark theme eka use karanna one.
+                const { isAuthenticated, toggleAuth} = authContext; //authContext kiyana context eka use karanna one nam this.context kiyana property eka use karanna one.
+                        
+                        
+              return (
+                <nav style={{ color: theme.syntax, background: theme.ui }}>
+                      <h1>Context App</h1>
+                      <div onClick={toggleAuth}>{ isAuthenticated?'Logged in':'Logged out'}</div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
