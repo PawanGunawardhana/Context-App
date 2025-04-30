ffc4c9db3554e3835886d4d40d54a6f2f60b4758
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-04-30 18:55:39 +0530

    Updated BookList.js with using useContext hook to access contexts.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react'
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component, useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark; //isLightTheme kiyana property eka true nam light theme eka use karanna one. naththam dark theme eka use karanna one.
-        return (
-            <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
-               <ul>
-                <li style={{background: theme.ui}}>Madol Doowa</li>
-                <li style={{background: theme.ui}}>Hath Pana</li>
-                <li style={{background: theme.ui}}>Heensaraya</li>
-               </ul>
-            </div>
-        );
-    }
-}
- 
+// class BookList extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark; //isLightTheme kiyana property eka true nam light theme eka use karanna one. naththam dark theme eka use karanna one.
+//         return (
+//             <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+//                <ul>
+//                 <li style={{background: theme.ui}}>Madol Doowa</li>
+//                 <li style={{background: theme.ui}}>Hath Pana</li>
+//                 <li style={{background: theme.ui}}>Heensaraya</li>
+//                </ul>
+//             </div>
+//         );
+//     }
+// }
+//export default BookList;
+
+//using useContext hook inside a functional component to access context.
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Madol Doowa</li>
+        <li style={{ background: theme.ui }}>Hath Pana</li>
+        <li style={{ background: theme.ui }}>Heensaraya</li>
+      </ul>
+    </div>
+  );
+};
+
 export default BookList;
