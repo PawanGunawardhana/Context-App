24288ae2630b10fd87ada3f52b70a154dda4bcd7
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-04-30 19:55:37 +0530

    Accessed context and multiple contexts using useCreate hook.

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext; //context eka use karanna one nam this.context kiyana property eka use karanna one.
+// class ThemeToggle extends Component {
+//   static contextType = ThemeContext; //context eka use karanna one nam this.context kiyana property eka use karanna one.
 
-  render() {
-    const { toggleTheme } = this.context; //context eka use karanna one nam this.context kiyana property eka use karanna one.
-    return (<button onClick={toggleTheme}>Toggle a Theme</button>);
-  }
+//   render() {
+//     const { toggleTheme } = this.context; //context eka use karanna one nam this.context kiyana property eka use karanna one.
+//     return (<button onClick={toggleTheme}>Toggle a Theme</button>);
+//   }
+// }
+
+// export default ThemeToggle;
+
+
+
+//useContext hook for accessing context in a functional component.
+const ThemeToggle = () => {
+
+  const {toggleTheme} = useContext(ThemeContext)
+  return (
+    <button onClick={toggleTheme}>Toggle a Theme</button>
+  );
 }
-
+ 
 export default ThemeToggle;
